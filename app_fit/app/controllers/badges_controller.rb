a308d68c2eaa33a432c2ba4029dab18ce8fd1619
Francisco <fjcastillo5@miuandes.cl>
2025-10-16 01:00:32 -0300

    badges-index

--- app_fit/app/controllers/badges_controller.rb
+++ app_fit/app/controllers/badges_controller.rb
@@ -1,5 +1,5 @@
 class BadgesController < ApplicationController
-    before_action :set_badge, only: [:show, :edit, :update, :destroy]
+    before_action :set_badge, only: [:show]
 
     def index
         @badges = Badge.all
