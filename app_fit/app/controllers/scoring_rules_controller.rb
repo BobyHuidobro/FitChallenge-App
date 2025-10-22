7cc24c64b3e63f61d763a57de1f092a554acb7e7
Domingo Huidobro <d.huidobro@miuandes.cl>
2025-10-22 00:19:56 -0300

    controller scoring rules

--- app_fit/app/controllers/scoring_rules_controller.rb
+++ app_fit/app/controllers/scoring_rules_controller.rb
@@ -1,5 +1,5 @@
 class ScoringRulesController < ApplicationController
-    before_action :set_scoring_rule, only: [:show]
+    before_action :set_scoring_rule, only: [:show, :edit, :update, :destroy]
 
     def index
         @scoring_rules = ScoringRule.all
@@ -8,8 +8,41 @@ class ScoringRulesController < ApplicationController
     def show
     end
 
+    def new
+        @scoring_rule = current_user.scoring_rules.build
+    end
+
+    def create
+        @scoring_rule = current_user.scoring_rules.build scoring_rule_params
+        if @scoring_rule.save
+            redirect_to scoring_rule_path(@scoring_rule)
+        else
+            redirect_to new_scoring_rule_path, alert: @scoring_rule.errors.full_messages.to_sentence
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @scoring_rule.update scoring_rule_params
+            redirect_to scoring_rule_path(@scoring_rule), notice: "Scoring Rule updated successfully."
+        else
+            redirect_to edit_scoring_rule_path(@scoring_rule), alert: @scoring_rule.errors.full_messages.to_sentence
+        end
+    end
+
+    def destroy
+        @scoring_rule.destroy
+        redirect_to scoring_rules_path
+    end
+
     private
     def set_scoring_rule
         @scoring_rule = ScoringRule.find params[:id]
+    end
+
+    def scoring_rule_params
+        params.require(:scoring_rule).permit(:rule_type, :description)
     end
 end
